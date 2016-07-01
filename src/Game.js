a58ec669092c284c8bdda10b3fba6d95df9332fe
Simon Breiter <hello@simonbreiter.ch>
2016-07-01 19:07:22 +0200

    Add function that players can combine multiple gameobjects, update game instructions accordingly, begin to refactor code, add possibility to exit game

--- src/Game.js
+++ src/Game.js
@@ -6,45 +6,99 @@ var Util = require('./Util');
 
 var json = JSON.parse(fs.readFileSync('./game_instructions.json', 'utf8'));
 var rl = readline.createInterface(process.stdin, process.stdout);
-var endstate = 'q3';
-var currentState = 'q1';
+var endstate = 'q2';
+var currentState = 'q0';
+var output = [];
+
+function init() {
+    output.push(json[currentState]['prompt']);
+    renderOutput();
+    clearOutput();
+}
 
 rl.on('line', function(input) {
-    Util.clearScreen();
-    gameRound(input);
-    rl.prompt();
+    if(input != "exit") {
+        // Play one round with input
+        gameLoop(input);
+        // Wait for next input
+        rl.prompt();
+    } else {
+        exit();
+    }
 }).on('close', function() {
     exit();
 });
 
-function init() {
+function renderOutput() {
     Util.clearScreen();
-    // Prompt of initial state
-    console.log(json[currentState]['prompt']);
+    console.log(output.join("\n"));
+    clearOutput();
 }
 
-function gameRound(input) {
-    var action = input.split(" ")[0];
-    var gameObject = input.split(" ")[1];
+function clearOutput() {
+    output = [];
+}
+
+function gameLoop(input) {
+    var action = input.split(" ")[0]; // Example action: use
+    var gameObject1 = input.split(" ")[1]; // Example gameObject: axe
+    var combine = input.split(" ")[2]; // Example: with
+    var gameObject2 = input.split(" ")[3]; // Example: door
     var possibleActions = Object.keys(json[currentState]['actions']);
+
+    // Check if action is in possible actions
     if(Util.isInArray(action, possibleActions)) {
         var possibleGameObjects = Object.keys(json[currentState]['actions'][action]);
-        if(Util.isInArray(gameObject, possibleGameObjects)) {
-            Util.clearScreen();
-            currentState = json[currentState]['actions'][action][gameObject];
+        // If user typed a valid action, check if its also on a valid gameobject
+        if(Util.isInArray(gameObject1, possibleGameObjects)) {
+
+            // If a user wants to combine a valid gameobject
+            if(combine === "with" && gameObject2 != undefined && json[currentState]['actions'][action][gameObject1]['with'] != undefined) {
+                var possibleGameObjectsToCombine = Object.keys(json[currentState]['actions'][action][gameObject1]['with']);
+                if(Util.isInArray(gameObject2, possibleGameObjectsToCombine)) {
+                    if(json[currentState]['actions'][action][gameObject1]['with'][gameObject2]["nextState"] != undefined) {
+                        currentState = json[currentState]['actions'][action][gameObject1]['with'][gameObject2]['nextState'];
+                        output.push(json[currentState]['prompt']);
+                    } else if(json[currentState]['actions'][action][gameObject1]['with'][gameObject2]["prompt"] != undefined) {
+                        output.push(json[currentState]['actions'][action][gameObject1]['with'][gameObject2]["prompt"]);
+                    } else{
+                        invalidCommand();
+                    }
+                } else {
+                    invalidCommand();
+                }
+            }
+            // If a prompt on gameobject exists, push it to output array
+            else if(json[currentState]['actions'][action][gameObject1]["prompt"] != undefined) {
+                output.push(json[currentState]['actions'][action][gameObject1]['prompt']);
+            }
+
+            // If this gameObject change state, update current state with new state
+            else if(json[currentState]['actions'][action][gameObject1]["nextState"] != undefined) {
+                currentState = json[currentState]['actions'][action][gameObject1]["nextState"];
+                output.push(json[currentState]['prompt']);
+            }
+            else {
+                invalidCommand();
+            }
         } else {
             invalidCommand();
         }
     } else {
         invalidCommand();
     }
-    // Print prompt of next state
-    console.log(json[currentState]['prompt']);
+
+    // Render output and cleanup array afterwards
+    renderOutput();
+    if(currentState === endstate) {
+        exit();
+    }
+
 }
 
 function invalidCommand() {
     Util.clearScreen();
-    console.log("I can't do that.")
+    output.push("You can't do that.")
 }
 
 function exit() {
