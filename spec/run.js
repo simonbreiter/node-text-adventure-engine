097619f25484ee81343138d770fe15570bce263f
Simon Breiter <hello@simonbreiter.com>
2017-07-07 13:28:20 +0200

    Add coverage, streamline test pipeline

--- spec/run.js
+++
@@ -1,7 +0,0 @@
-import Jasmine from 'jasmine'
-
-// Jasmine and ES6: https://gist.github.com/mauvm/172878a9646095d03fd7
-
-const jasmine = new Jasmine()
-jasmine.loadConfigFile('./spec/support/jasmine.json')
-jasmine.execute()
